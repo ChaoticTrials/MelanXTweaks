c8007e5181f8b4a684dff670fb96e810ff8e9613
MelanX <meissi97@me.com>
2018-03-08 17:23:21 +0100

    beep boop boop beep

--- src/main/java/de/melanx/mxtweaks/mxtweaks.java
+++ src/main/java/de/melanx/mxtweaks/mxtweaks.java
@@ -15,9 +15,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(   modid = mxtweaks.MODID,
-        version = "b-0.1",
-        name = "MelanX Tweaks")
+@Mod(modid = mxtweaks.MODID, version = "b-0.1", name = "MelanX Tweaks")
 
 public class mxtweaks {
 
