3212b1f84a9382b036f6a481bf0f8f3d485c127f
MelanX <info@melanx.de>
2021-04-03 15:09:28 +0200

    remove ability to generate gog world on server

--- src/main/java/de/melanx/mxtweaks/MelanXTweaks.java
+++ src/main/java/de/melanx/mxtweaks/MelanXTweaks.java
@@ -3,16 +3,12 @@ package de.melanx.mxtweaks;
 import de.melanx.mxtweaks.loot.ModLootFunctionManager;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.dedicated.DedicatedServer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -42,12 +38,5 @@ public class MelanXTweaks {
 
     private void clonePlayer(PlayerEvent.Clone event) {
         event.getPlayer().getPersistentData().putBoolean(ITEM_RECEIVED_TAG, event.getOriginal().getPersistentData().getBoolean(ITEM_RECEIVED_TAG));
-    }
-
-    @SubscribeEvent
-    public void serverAboutToStart(FMLServerAboutToStartEvent event) {
-        if (event.getServer() instanceof DedicatedServer) {
-            WorldTypeUtil.setupForDedicatedServer((DedicatedServer) event.getServer());
-        }
     }
 }
