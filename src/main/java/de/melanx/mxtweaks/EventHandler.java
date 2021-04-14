22930136ad42c7b4ef8a78ea52d9bf2f3bcfb1ab
MelanX <info@melanx.de>
2021-04-14 12:58:20 +0200

    add config for player join message

--- src/main/java/de/melanx/mxtweaks/EventHandler.java
+++ src/main/java/de/melanx/mxtweaks/EventHandler.java
@@ -3,7 +3,10 @@ package de.melanx.mxtweaks;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class EventHandler {
@@ -16,6 +19,16 @@ public class EventHandler {
                 ((ServerPlayerEntity) event.player).connection.floatingTickCount = 0;
                 ((ServerPlayerEntity) event.player).connection.vehicleFloatingTickCount = 0;
             }
+        }
+    }
+
+    @SubscribeEvent
+    public void onEntityJoin(EntityJoinWorldEvent event) {
+        if (event.getEntity() instanceof ServerPlayerEntity) {
+            ServerPlayerEntity player = (ServerPlayerEntity) event.getEntity();
+            MelanXTweaks.Config.startupMessages.get().forEach(s -> {
+                player.sendStatusMessage(new StringTextComponent(s).mergeStyle(TextFormatting.RED), false);
+            });
         }
     }
 }
--- src/main/java/de/melanx/mxtweaks/MelanXTweaks.java
+++ src/main/java/de/melanx/mxtweaks/MelanXTweaks.java
@@ -6,11 +6,17 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.Collections;
+import java.util.List;
 
 @Mod(MelanXTweaks.MODID)
 public class MelanXTweaks {
@@ -23,8 +29,8 @@ public class MelanXTweaks {
         ModLootFunctionManager.init();
         MinecraftForge.EVENT_BUS.addListener(this::onPlayerJoin);
         MinecraftForge.EVENT_BUS.addListener(this::clonePlayer);
-        MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(new EventHandler());
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.SERVER_CONFIG);
     }
 
     private void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
@@ -38,5 +44,23 @@ public class MelanXTweaks {
 
     private void clonePlayer(PlayerEvent.Clone event) {
         event.getPlayer().getPersistentData().putBoolean(ITEM_RECEIVED_TAG, event.getOriginal().getPersistentData().getBoolean(ITEM_RECEIVED_TAG));
+    }
+
+    public static class Config {
+
+        public static final ForgeConfigSpec SERVER_CONFIG;
+        private static final ForgeConfigSpec.Builder SERVER_BUILDER = new ForgeConfigSpec.Builder();
+
+        static {
+            init(SERVER_BUILDER);
+            SERVER_CONFIG = SERVER_BUILDER.build();
+        }
+
+        public static ForgeConfigSpec.ConfigValue<List<? extends String>> startupMessages;
+
+        public static void init(ForgeConfigSpec.Builder builder) {
+            startupMessages = builder.comment("Messages which will be shown on each world join.")
+                    .defineList("messages", Collections.emptyList(), obj -> obj instanceof String);
+        }
     }
 }
